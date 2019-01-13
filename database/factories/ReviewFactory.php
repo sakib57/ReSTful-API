17f83fa6601510f4736fc9d6c87e1b1f163e9c39
Nazmus Sakib <sakib.tnt@gmail.com>
2019-01-13 10:35:26 +0600

    Create migrations

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\Product;
 
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
-        //
+    	'product_id'=>function(){
+    		return Product::all()->random();
+    	},
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,50)
     ];
 });
